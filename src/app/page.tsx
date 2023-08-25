b45559f2032ec847a0f534ff42e4c133a0cd0cc6
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-25 16:19:46 -0600

    get all tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { prisma } from "@/libs/prisma";
 
-function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+async function loadTasks() {
+  const tasks = await prisma.task.findMany();
+  return tasks;
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTasks();
+  console.log(tasks);
+  return (
+    <div className="grid grid-cols-3 gap-3 mt-5">
+      {tasks.map((task) => (
+        <div key={task.id} className="bg-gray-900 p-3">
+          <h1 className="font-bold text-xl">{task.title}</h1>
+          <p className="to-slate-300">{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default HomePage;
